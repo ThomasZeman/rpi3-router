7f3fff6f959ca95a05e0c442e87af903e4f65fb2
Thomas Zeman <git@thomas-zeman.com>
2018-09-12 22:13:14 +1000

    Removed footer

--- openvpn-wan/runopenvpn.sh
+++ openvpn-wan/runopenvpn.sh
@@ -17,6 +17,9 @@ touch /etc/resolv.1.conf
 
 /usr/sbin/dhclient eth0
 
+# This is annoying to have but necessary with the basic openvpn setup
+# openvpn server side would need to know how to route 10.1.0.0/16 pakets
+# It should be enough to masquerade on the end which owns the public ip
 iptables -t nat -A POSTROUTING -s 10.1.0.0/16 -j MASQUERADE
 
 exec /usr/sbin/openvpn --config /data/client.ovpn
