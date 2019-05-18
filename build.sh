1093e31d49aab5c6369337ae3ba5d357bbdec421
Thomas Zeman <git@thomas-zeman.com>
2019-05-19 09:21:34 +1000

    Added to build all script

--- build.sh
+++ build.sh
@@ -4,9 +4,15 @@ cd hostapd
 ./build.sh
 cd ../dhcpd
 ./build.sh
+cd ../eth-lan
+./build.sh
+cd ../router
+./build.sh
 cd ../base-wan
 ./build.sh
-cd ../openvpn-wan
+cd ../passthrough-wan
+./build.sh
+cd ../wireguard-wan
 ./build.sh
 cd ../eth-wan
 ./build.sh
